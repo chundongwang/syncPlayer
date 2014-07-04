bb128b27ea9e1518e2ab03f54c66979337fe41cd
JewelryWolf <chundongwang@gmail.com>
2014-07-03 23:08:55 -0700

    another hello world

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
+# Copyright 2014 Chundong Wang
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Empty hello world!')
+from flask import Flask
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+app = Flask(__name__, static_folder='static')
+app.config.update(dict(
+    DEBUG=True,
+    SECRET_KEY='f!>FEFGVvH7,%[yIjZUu79!9~k9nfi'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+@app.route('/')
+def main():
+    template = 'index.html'
+    return render_template(template)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Return a custom 404 error."""
+    return Response('Sorry, nothing at this URL.', 404)
+
+if __name__ == '__main__':
+    app.run()
+
+#class MainHandler(webapp2.RequestHandler):
+#    def get(self):
+#        self.response.write('Empty hello world!')
+
+#app = webapp2.WSGIApplication([
+#    ('/', MainHandler)
+#], debug=True)
